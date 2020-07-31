0dfd7ed74430b9802f1854c2c6defc335c2f729c
filigreti <aolfiligre@gmail.com>
2020-07-31 17:45:35 +0100

    WIP

--- src/views/Dashboard/SubEvents.vue
+++ src/views/Dashboard/SubEvents.vue
@@ -3,13 +3,11 @@
     <main>
       <div @click="$router.go(-1)" class="flex items-center cursor-pointer">
         <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 443.52 443.52">
-          <path
-            d="M143.5 221.86L336.22 29.13A17.07 17.07 0 00312.09 5L107.3 209.8a17.07 17.07 0 000 24.13l204.8 204.8a17.07 17.07 0 0024.14-24.13L143.49 221.86z"
-          />
+          <path d="M143.5 221.86L336.22 29.13A17.07 17.07 0 00312.09 5L107.3 209.8a17.07 17.07 0 000 24.13l204.8 204.8a17.07 17.07 0 0024.14-24.13L143.49 221.86z" />
         </svg>
         <h1 class="text-2xl pl-2 tracking-wide font-bold">
           Events /
-          <span class="text-blue-600">{{event.event_title}}</span>
+          <span class="text-blue-600">{{ event.event_title }}</span>
         </h1>
       </div>
     </main>
@@ -19,84 +17,37 @@
         <div class="grid lg:grid-cols-2 lg:gap-5 row-gap-5">
           <div class="flex items-center">
             <h1 class="text-base font-light text-gray-700 tracking-wide">Title</h1>
-            <input
-              required
-              v-model="event.event_title"
-              class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal"
-              type="string"
-              placeholder="Title"
-            />
+            <input required v-model="event.event_title" class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal" type="string" placeholder="Title" />
           </div>
           <div class="flex items-center">
             <h1 class="text-base font-light text-gray-700 tracking-wide">Time</h1>
-            <input
-              required
-              v-model="event.event_time"
-              class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal"
-              type="time"
-              placeholder="Time"
-            />
+            <input required v-model="event.event_time" class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal" type="time" placeholder="Time" />
           </div>
           <div class="flex items-center justify-between">
             <h1 class="text-base font-light text-gray-700 tracking-wide flex">Start Date</h1>
-            <input
-              required
-              v-model="event.event_start_date"
-              class="bg-gray-100 font-light ml-10 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block appearance-none leading-normal"
-              type="date"
-              placeholder="jane@example.com"
-            />
+            <input required v-model="event.event_start_date" class="bg-gray-100 font-light ml-10 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block appearance-none leading-normal" type="date" placeholder="jane@example.com" />
           </div>
           <div class="flex items-center justify-between">
             <h1 class="text-base font-light text-gray-700 tracking-wide">End Date</h1>
-            <input
-              required
-              v-model="event.event_end_date"
-              class="bg-gray-100 font-light ml-10 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block appearance-none leading-normal"
-              type="date"
-              placeholder="jane@example.com"
-            />
+            <input required v-model="event.event_end_date" class="bg-gray-100 font-light ml-10 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block appearance-none leading-normal" type="date" placeholder="jane@example.com" />
           </div>
         </div>
         <div class="flex items-center mt-5">
           <h1 class="text-base font-light text-gray-700 tracking-wide">Venue</h1>
-          <input
-            required
-            v-model="event.address"
-            class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal"
-            type="string"
-            placeholder="jane@example.com"
-          />
+          <input required v-model="event.address" class="bg-gray-100 font-light ml-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 pl-4 lg:px-4 block w-full appearance-none leading-normal" type="string" placeholder="jane@example.com" />
         </div>
         <div class="mt-5 flex lg:flex-row flex-col">
-          <div
-            @click="$refs.file.click()"
-            class="h-32 border w-32 rounded-lg flex justify-center items-center relative"
-          >
-            <svg
-              v-if="event.event_flyer === null"
-              class="fill-current text-gray-400 w-8 h-8"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                d="M494.14 43.16H17.86C8 43.16 0 51.16 0 61.02v389.96c0 9.86 8 17.86 17.86 17.86h476.28c9.87 0 17.86-8 17.86-17.86V61.02c0-9.86-8-17.86-17.86-17.86zm-17.86 389.96H35.72V78.88h440.56v354.24z"
-              />
-              <path
-                d="M191.1 122.34a65.57 65.57 0 00-65.48 65.5 65.57 65.57 0 0065.49 65.48 65.56 65.56 0 0065.48-65.49 65.57 65.57 0 00-65.48-65.49zm0 95.26a29.8 29.8 0 01-29.76-29.77 29.8 29.8 0 0129.77-29.76 29.8 29.8 0 0129.76 29.76 29.8 29.8 0 01-29.76 29.77zM352.71 269.22a17.84 17.84 0 00-17.82 1.74l-229.8 164.91 20.82 29.03 221.4-158.88 139.42 63.57 14.82-32.5-148.84-67.87z"
-              />
+          <div @click="$refs.file.click()" class="h-32 border w-32 rounded-lg flex justify-center items-center relative">
+            <svg v-if="event.event_flyer === null" class="fill-current text-gray-400 w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M494.14 43.16H17.86C8 43.16 0 51.16 0 61.02v389.96c0 9.86 8 17.86 17.86 17.86h476.28c9.87 0 17.86-8 17.86-17.86V61.02c0-9.86-8-17.86-17.86-17.86zm-17.86 389.96H35.72V78.88h440.56v354.24z" />
+              <path d="M191.1 122.34a65.57 65.57 0 00-65.48 65.5 65.57 65.57 0 0065.49 65.48 65.56 65.56 0 0065.48-65.49 65.57 65.57 0 00-65.48-65.49zm0 95.26a29.8 29.8 0 01-29.76-29.77 29.8 29.8 0 0129.77-29.76 29.8 29.8 0 0129.76 29.76 29.8 29.8 0 01-29.76 29.77zM352.71 269.22a17.84 17.84 0 00-17.82 1.74l-229.8 164.91 20.82 29.03 221.4-158.88 139.42 63.57 14.82-32.5-148.84-67.87z" />
               <path d="M9.63 276.9l16.47-31.7 209.58 108.96-16.47 31.69z" />
             </svg>
             <img v-else :src="event.event_flyer" class="h-32 w-32 object-contain" alt />
 
             <div class="text-xs text-gray-600 absolute bottom-0 -mb-2 bg-white px-2">Upload Image</div>
           </div>
-          <textarea
-            v-model="event.description"
-            placeholder="Description"
-            type="text"
-            class="h-32 flex-grow text-sm p-2 lg:mt-0 mt-5 rounded-lg border lg:ml-10 focus:outline-none focus:shadow-outline"
-          />
+          <textarea v-model="event.description" placeholder="Description" type="text" class="h-32 flex-grow text-sm p-2 lg:mt-0 mt-5 rounded-lg border lg:ml-10 focus:outline-none focus:shadow-outline" />
         </div>
       </div>
       <!-- <div class="mt-5 flex justify-end">
@@ -107,11 +58,7 @@
     </form>
     <div class="lg:px-2 overflow-x-auto mt-8">
       <div>
-        <input
-          v-model="search"
-          type="text"
-          class="border py-2 rounded-full w-64 mb-3 focus:outline-none pl-5 text-sm text-gray-600 font-light"
-        />
+        <input v-model="search" type="text" class="border py-2 rounded-full w-64 mb-3 focus:outline-none pl-5 text-sm text-gray-600 font-light" />
       </div>
       <table class="table-auto w-full">
         <thead>
@@ -128,46 +75,37 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            class="text-xs font-light tracking-wide text-gray-600"
-            v-for="(person,index) in currentPost"
-            :key="index"
-          >
+          <tr class="text-xs font-light tracking-wide text-gray-600" v-for="(person, index) in currentPost" :key="index">
             <td class="border px-4 py-2">{{ index + 1 }}</td>
             <td class="border px-4 py-2">
-              <div class="w-32">{{person.attendee_first_name}} {{person.attendee_last_name}}</div>
+              <div class="w-32">{{ person.attendee_first_name }} {{ person.attendee_last_name }}</div>
             </td>
             <td class="border px-4 py-2">
-              <div class="w-24">{{person.attendee_mobile_number}}</div>
+              <div class="w-24">{{ person.attendee_mobile_number }}</div>
             </td>
-            <td class="border px-4 py-2">{{person.attendee_email_address}}</td>
+            <td class="border px-4 py-2">{{ person.attendee_email_address }}</td>
 
-            <td class="border px-4 py-2">{{person.attendee_state}},{{person.attendee_country}}</td>
-            <td class="border px-4 py-2">{{person.attendee_state}},{{person.workshop_class}}</td>
+            <td class="border px-4 py-2">{{ person.attendee_state }},{{ person.attendee_country }}</td>
+            <td class="border px-4 py-2">{{ person.attendee_state }},{{ person.workshop_class }}</td>
             <td class="border px-6 py-2">
-              <div class="w-24">{{person.bible_study_group_no}}</div>
+              <div class="w-24">{{ person.bible_study_group_no }}</div>
             </td>
             <td class="border px-8 py-2">
-              <div class="w-20">{{person.reg_no}}</div>
+              <div class="w-20">{{ person.reg_no }}</div>
             </td>
-            <td class="border px-8 py-2">{{person.attendee_gender}}</td>
+            <td class="border px-8 py-2">{{ person.attendee_gender }}</td>
           </tr>
         </tbody>
       </table>
     </div>
     <div class="flex mt-5">
-      <div
-        :class="{'red border-blue-400 ': page == currentPage}"
-        class="border py-2 px-2 m-1 text-blue-500 hover:bg-gray-200 cursor-pointer"
-        v-for="page in pagination"
-        :key="page"
-        @click="paginate(page)"
-      >{{page}}</div>
+      <div :class="{ 'red border-blue-400 ': page == currentPage }" class="border py-2 px-2 m-1 text-blue-500 hover:bg-gray-200 cursor-pointer" v-for="page in pagination" :key="page" @click="paginate(page)">{{ page }}</div>
     </div>
   </main>
 </template>
 
 <script>
+import Fuse from "fuse.js";
 export default {
   data() {
     return {
@@ -180,8 +118,8 @@ export default {
 
       event: {
         event_title: "",
-        event_attendees: []
-      }
+        event_attendees: [],
+      },
     };
   },
   computed: {
@@ -197,17 +135,31 @@ export default {
         let lastPostIndex = this.currentPage * this.total;
         let firstPostIndex = lastPostIndex - this.total;
 
-        return this.event.event_attendees
-          .filter(k => {
-            return (
-              k.attendee_first_name.toLowerCase() ||
-              k.attendee_last_name
-                .toLowerCase() // k.attendee_last_name || // k.attendee_first_name ||
-                // k.attendee_email_address
-                .match(this.search.toLowerCase())
-            );
-          })
-          .slice(firstPostIndex, lastPostIndex);
+        const options = {
+          keys: ["attendee_first_name", "attendee_last_name", "attendee_email_address", "reg_no"],
+        };
+        let k = this.event.event_attendees;
+        const fuse = new Fuse(k, options);
+        if (this.search == "") {
+          return k.slice(firstPostIndex, lastPostIndex);
+        } else {
+          let search = [];
+          fuse.search(this.search).map((x) => search.push(x.item));
+          return search.slice(firstPostIndex, lastPostIndex);
+        }
+        // return (
+        //   this.event.event_attendees
+        //     // .filter((k) => {
+        //     //   return (
+        //     //     k.reg_no
+        //     //       .toLowerCase()
+        //     //       // k.attendee_last_name || // k.attendee_first_name ||
+        //     //       // k.attendee_email_address
+        //     //       .match(this.search.toLowerCase())
+        //     //   );
+        //     // })
+        //     .slice(firstPostIndex, lastPostIndex)
+        // );
       }
 
       //     return pageNumbers.filter(k => {
@@ -218,23 +170,25 @@ export default {
       //     k.attendee_email_address
       //   ).match(this.search);
       // });
-    }
+    },
   },
 
   methods: {
     paginate(x) {
       this.currentPage = x;
-    }
+    },
   },
   mounted() {
-    this.event = this.$store.getters.getEvents.results.find(x => {
+    this.event = this.$store.getters.getEvents.results.find((x) => {
       return x.slug == this.$route.params.events;
     });
     this.totalLength = this.event.event_attendees.length;
 
     this.totalPage = this.event.event_attendees.length;
-    console.log(this.pagination);
-  }
+  },
+  updated() {
+    console.log(this.currentPost);
+  },
 };
 </script>
 
